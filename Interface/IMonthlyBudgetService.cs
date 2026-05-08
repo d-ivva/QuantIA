439c387dca3137cf84ec8c4b374e2fcc0d5de2d3
Arthur Bauer Cardoso <arthur.cardoso@01tec.com.br>
2026-05-08 00:08:25 -0300

    Branch: v1.1/MetasGasto
    - Adição de MonthlyBudget (Metas de Gastos)

---
+++ Interface/IMonthlyBudgetService.cs
@@ -0,0 +1,13 @@
+using QuantIA.Models;
+
+namespace QuantIA.Interface;
+
+public interface IMonthlyBudgetService
+{
+    Task<MonthlyBudget> Criar(MonthlyBudget request);
+    Task<List<MonthlyBudget>> Listar();
+    Task<MonthlyBudget?> BuscarPorId(int id);
+    Task<MonthlyBudget?> BuscarPorMesAno(int month, int year);
+    Task Atualizar(int id, MonthlyBudget request);
+    Task Deletar(int id);
+}
